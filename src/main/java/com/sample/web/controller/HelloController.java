e8e06da713e5a974d005db4cf0460d5a8f1aa442
Aditya <adityasrivastava010@gmail.com>
2016-06-28 22:42:42 +0530

    update

---
+++ src/main/java/com/sample/web/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.sample.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value="/hello", method=RequestMethod.GET)
+    public String helloWorld(Model model) {
+    	if(model == null){
+    		System.out.println("ITs null");
+    	}
+        model.addAttribute("message", "Hello World!");
+        return "hello";
+    }
+}
